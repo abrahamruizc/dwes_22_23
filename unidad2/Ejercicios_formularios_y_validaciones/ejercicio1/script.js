ffafc81410e823c158a539cce5ee30d5302c083c
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-10-11 13:23:09 +0200

    haciendo ejercicios formularios 1

---
+++ unidad2/Ejercicios_formularios_y_validaciones/ejercicio1/script.js
@@ -0,0 +1,13 @@
+function validar (){
+
+    const $Nombre = document.getElementById("nombre");
+    const $Apellidos = document.getElementById("apellidos");
+    const $Telefono = document.getElementById("telefono");
+    const $Escribe = document.getElementById("escribe");
+    const $Hora = document.getElementById("hora");
+
+
+
+
+
+}
