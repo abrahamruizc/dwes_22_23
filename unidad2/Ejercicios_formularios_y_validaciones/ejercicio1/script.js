dd3f2e3bec28052418b0d611a30d07adfaacb2c9
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-10-11 17:18:15 +0200

    haciendo formulario mas

--- unidad2/Ejercicios_formularios_y_validaciones/ejercicio1/script.js
+++ unidad2/Ejercicios_formularios_y_validaciones/ejercicio1/script.js
@@ -1,5 +1,6 @@
-function validar (){
-
+function validar(){
+    const $Form = document.getElementById("ej1");
+    const $DNI = document.getElementById("dni");
     const $Nombre = document.getElementById("nombre");
     const $Apellidos = document.getElementById("apellidos");
     const $Telefono = document.getElementById("telefono");
@@ -7,7 +8,31 @@ function validar (){
     const $Hora = document.getElementById("hora");
 
 
+    function Genera (){
+        const dni = $DNI.value;
+        const nombre = $Nombre.value;
+        const apellidos = $Apellidos.value;
+        const telefono = $Telefono.value;
+        const escribe = $Escribe.value;
+        const hora = $Hora.value;
+    }
+    
 
 
+    function compruebaDNI (dni) {
+        const validacion = /^[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][A-Z]$/;
+        return validacion.test(dni);
+    }
+
+    function compruebaNombre (nombre) {
+        const validacion = /^(([a-zA-ZáéíóúñÑÁÉÍÓÚ]+)|([a-zA-ZáéíóúñÑÁÉÍÓÚ]+\s[a-zA-ZáéíóúñÑÁÉÍÓÚ]+))$/;
+        return validacion.test(nombre);
+    }
+
+    function compruebaApellidos (apellido) {}
+
+    function compruebaTelefono (telefono) {}
+
+    function compruebaHora (hora) {}
 
 }
