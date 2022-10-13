278a2b06701e6f485b6e81b95df0d43dd64a154b
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-10-13 17:12:38 +0200

    haciendo formulario3

--- unidad2/Ejercicios_formularios_y_validaciones/ejercicio2/script.js
+++ unidad2/Ejercicios_formularios_y_validaciones/ejercicio2/script.js
@@ -1,46 +1,37 @@
-const $Form = document.getElementById("ej1");
-const $DNI = document.getElementById("dni");
+const $Form = document.getElementById("ej2");
 const $Nombre = document.getElementById("nombre");
 const $Apellidos = document.getElementById("apellidos");
-const $Telefono = document.getElementById("telefono");
-const $Escribe = document.getElementById("escribe");
-const $Hora = document.getElementById("hora");
+const $Email = document.getElementById("email");
+const $Web = document.getElementById("web");
 
 function handleSubmit(e) {
   e.preventDefault();
-  const dni = $DNI.value;
+  
   const nombre = $Nombre.value;
   const apellidos = $Apellidos.value;
-  const telefono = $Telefono.value;
-  const escribe = $Escribe.value;
-  const hora = $Hora.value;
+  const email = $Email.value;
+  const web = $Web.value;
 
-  if (dni == false || compruebaDNI(dni) == false) {
-    alert("el dni no es valido");
-  } else if (nombre == false || compruebaNombre(nombre) == false) {
+  if (nombre == false || compruebaNombre(nombre) == false) {
     alert("el nombre no es valido");
   } else if (apellidos == false || compruebaApellidos(apellidos) == false) {
     alert("el apellido no es valido");
-  } else if (telefono == false || compruebaTelefono(telefono) == false) {
-    alert("el telefono no es valido");
+  } else if (email == false || compruebaemail(email) == false) {
+    alert("el email no es valido");
+  } else if (web == false || compruebaweb(web) == false){
+    alert("la web no es valida");
   } else {
     let registrarUsuario = {
-      dni: dni,
       nombre: nombre,
       apellidos: apellidos,
-      telefono: telefono,
-      escribe: escribe,
-      hora: hora,
+      email: email,
+      web: web
     };
     let nuevoUsuario = JSON.stringify(registrarUsuario);
     console.log(nuevoUsuario);
   }
 }
 
-function compruebaDNI(dni) {
-  const validacion = /^[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][A-Z]$/;
-  return validacion.test(dni);
-}
 
 function compruebaNombre(nombre) {
   const validacion =
@@ -54,8 +45,14 @@ function compruebaApellidos(apellido) {
   return validacion.test(apellido);
 }
 
-function compruebaTelefono(telefono) {
-  const validacion = /^[+][3][4]+\s[0-9]{9}$/;
-  return validacion.test(telefono);
+function compruebaemail(email) {
+  const validacion = /^([a-zA-Z]+)[@]([a-z]+)[.][a-z]{3}$/;
+  return validacion.test(email);
 }
+
+function compruebaweb(web) {
+    const validacion = /^(https:)[/][/](www.[a-z]+[.][a-z]{3})$/;
+    return validacion.test(web);
+  }
+
 $Form.addEventListener("submit", handleSubmit);
