233d4f469f63bd98fe6535d37a1f3e8e4867970b
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2022-10-24 21:23:33 +0200

    examen

---
+++ unidad2/Examen UD2 Lenguaje/Ejercicio3/ejercicio3.js
@@ -0,0 +1,23 @@
+let numero = Math.floor( Math.random() * (50 - 1) + 1);
+console.log(numero);
+
+let pregunta= parseInt(prompt("numero"));
+console.log(pregunta);
+
+while(pregunta == isNaN() || pregunta == NaN){
+    pregunta = parseInt(prompt("introduce un numero"));
+    console.log(pregunta);
+
+    if (pregunta === numero){
+        break;
+    }
+
+}
+
+//document.write("");
+
+
+
+
+
+
